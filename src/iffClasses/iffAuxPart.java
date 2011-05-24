286b648041e5d99fb3c806912530545d3087ce98
Hedkandi <chreadie@gmail.com>
2011-05-24 16:19:08 +0200

    Changed variable names to reflect what they are used for Changed column titles

--- src/iffClasses/iffAuxPart.java
+++ src/iffClasses/iffAuxPart.java
@@ -7,9 +7,12 @@ package iffClasses;
 
 import gens.uData;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -19,38 +22,48 @@ public final class iffAuxPart extends iffClasses.iffBase {
 
     // base should be equal to iffBase.getColNum()
     private final int base = iffBase.base;
-    public short U14 = 0; 
+    public short Amount = 0; 
     public short U33 = 0; 
     public short U34 = 0; 
     public short U35 = 0; 
     public short U36 = 0; 
-    public short U37 = 0; 
-    public short U38 = 0; 
-    public short U39 = 0; 
-    public short U40 = 0; 
-    public short U41 = 0; 
-    public short U42 = 0; 
-    public short U43 = 0; 
-    public short U44 = 0; 
-    public short U45 = 0; 
-    public short U46 = 0; 
+    public byte Power = 0; 
+    public byte Control = 0;
+    public byte Accuracy = 0;
+    public byte Spin = 0;
+    public byte Curve = 0; 
+    public byte PowerSlot = 0;
+    public byte ControlSlot = 0;
+    public byte AccuracySlot = 0;
+    public byte SpinSlot = 0; 
+    public byte CurveSlot = 0;
+    public short DriveUp = 0; 
+    public short ItemDropUp = 0; 
+    public short ComboUp = 0; 
+    public short PangUp = 0; 
+    public short Exp = 0; 
     public short U47 = 0;
     
-    public String[] colNames = new String[] {  "NA",
+    public String[] colNames = new String[] {  "Amount",
                                         "NA",
                                         "NA",
                                         "NA",
                                         "NA",
-                                        "NA",
-                                        "NA",
-                                        "NA",
-                                        "NA",
-                                        "NA",
-                                        "NA",
-                                        "NA",
-                                        "NA",
-                                        "NA",
-                                        "NA",
+                                        "Power+",
+                                        "Control+",
+                                        "Accuracy+",
+                                        "Spin+",
+                                        "Curve+",
+                                        "Power Slot",
+                                        "Control Slot",
+                                        "Accuracy Slot",
+                                        "Spin Slot",
+                                        "Curve Slot",
+                                        "Yards+",
+                                        "Luck+",
+                                        "Wider Pangya",
+                                        "Pang+?",
+                                        "EXP+?",
                                         "NA"};
     
 
@@ -87,21 +100,21 @@ public final class iffAuxPart extends iffClasses.iffBase {
     @Override
     public void getItem(String[] inData) {
         super.getItem(inData);
-        this.U14 = uData.getShort(Integer.parseInt(inData[33]));
+        this.Amount = uData.getShort(Integer.parseInt(inData[33]));
         this.U33 = uData.getShort(Integer.parseInt(inData[34]));
         this.U34 = uData.getShort(Integer.parseInt(inData[35]));
         this.U35 = uData.getShort(Integer.parseInt(inData[36]));
         this.U36 = uData.getShort(Integer.parseInt(inData[37]));
-        this.U37 = uData.getShort(Integer.parseInt(inData[38]));
-        this.U38 = uData.getShort(Integer.parseInt(inData[39]));
-        this.U39 = uData.getShort(Integer.parseInt(inData[40]));
-        this.U40 = uData.getShort(Integer.parseInt(inData[41]));
-        this.U41 = uData.getShort(Integer.parseInt(inData[42]));
-        this.U42 = uData.getShort(Integer.parseInt(inData[43]));
-        this.U43 = uData.getShort(Integer.parseInt(inData[44]));
-        this.U44 = uData.getShort(Integer.parseInt(inData[45]));
-        this.U45 = uData.getShort(Integer.parseInt(inData[46]));
-        this.U46 = uData.getShort(Integer.parseInt(inData[47]));
+        //this.U37 = uData.getShort(Integer.parseInt(inData[38]));
+        //this.U38 = uData.getShort(Integer.parseInt(inData[39]));
+        //this.U39 = uData.getShort(Integer.parseInt(inData[40]));
+        //this.U40 = uData.getShort(Integer.parseInt(inData[41]));
+        //this.U41 = uData.getShort(Integer.parseInt(inData[42]));
+        this.DriveUp = uData.getShort(Integer.parseInt(inData[43]));
+        this.ItemDropUp = uData.getShort(Integer.parseInt(inData[44]));
+        this.ComboUp = uData.getShort(Integer.parseInt(inData[45]));
+        this.PangUp = uData.getShort(Integer.parseInt(inData[46]));
+        this.Exp = uData.getShort(Integer.parseInt(inData[47]));
         this.U47 = uData.getShort(Integer.parseInt(inData[48]));
     }
 
@@ -109,21 +122,26 @@ public final class iffAuxPart extends iffClasses.iffBase {
     public final void getItem(byte[] inData) throws IOException {
         try {
             super.getItem(inData);
-            this.U14 = uData.getShort(new byte[]{inData[144], inData[145]});
+            this.Amount = uData.getShort(new byte[]{inData[144], inData[145]});
             this.U33 = uData.getShort(new byte[]{inData[146], inData[147]});
             this.U34 = uData.getShort(new byte[]{inData[148], inData[149]});
             this.U35 = uData.getShort(new byte[]{inData[150], inData[151]});
             this.U36 = uData.getShort(new byte[]{inData[152], inData[153]});
-            this.U37 = uData.getShort(new byte[]{inData[154], inData[155]});
-            this.U38 = uData.getShort(new byte[]{inData[156], inData[157]});
-            this.U39 = uData.getShort(new byte[]{inData[158], inData[159]});
-            this.U40 = uData.getShort(new byte[]{inData[160], inData[161]});
-            this.U41 = uData.getShort(new byte[]{inData[162], inData[163]});
-            this.U42 = uData.getShort(new byte[]{inData[164], inData[165]});
-            this.U43 = uData.getShort(new byte[]{inData[166], inData[167]});
-            this.U44 = uData.getShort(new byte[]{inData[168], inData[169]});
-            this.U45 = uData.getShort(new byte[]{inData[170], inData[171]});
-            this.U46 = uData.getShort(new byte[]{inData[172], inData[173]});
+            this.Power = inData[154];
+            this.Control = inData[155];
+            this.Accuracy = inData[156];
+            this.Spin = inData[157];
+            this.Curve = inData[158];
+            this.PowerSlot = inData[159];
+            this.ControlSlot = inData[160];
+            this.AccuracySlot = inData[161];
+            this.SpinSlot = inData[162];
+            this.CurveSlot = inData[163];
+            this.DriveUp = uData.getShort(new byte[]{inData[164], inData[165]});
+            this.ItemDropUp = uData.getShort(new byte[]{inData[166], inData[167]});
+            this.ComboUp = uData.getShort(new byte[]{inData[168], inData[169]});
+            this.PangUp = uData.getShort(new byte[]{inData[170], inData[171]});
+            this.Exp = uData.getShort(new byte[]{inData[172], inData[173]});
             this.U47 = uData.getShort(new byte[]{inData[174], inData[175]});
         } catch (IOException ex) {
             throw new IOException(ex);
@@ -148,7 +166,7 @@ public final class iffAuxPart extends iffClasses.iffBase {
         else {
             switch (colIndex) {
                 case base:
-                    return uData.getInt(this.U14);
+                    return uData.getInt(this.Amount);
                 case (base+1):
                     return uData.getInt(this.U33);
                 case (base+2):
@@ -158,26 +176,36 @@ public final class iffAuxPart extends iffClasses.iffBase {
                 case (base+4):
                     return uData.getInt(this.U36);
                 case (base+5):
-                    return uData.getInt(this.U37);
+                    return uData.getShort(this.Power);
                 case (base+6):
-                    return uData.getInt(this.U38);
+                    return uData.getShort(this.Control);
                 case (base+7):
-                    return uData.getInt(this.U39);
+                    return uData.getShort(this.Accuracy);
                 case (base+8):
-                    return uData.getInt(this.U40);
+                    return uData.getShort(this.Spin);
                 case (base+9):
-                    return uData.getInt(this.U41);
+                    return uData.getShort(this.Curve);
                 case (base+10):
-                    return uData.getInt(this.U42);
+                    return uData.getShort(this.PowerSlot);
                 case (base+11):
-                    return uData.getInt(this.U43);
+                    return uData.getShort(this.ControlSlot);
                 case (base+12):
-                    return uData.getInt(this.U44);
+                    return uData.getShort(this.AccuracySlot);
                 case (base+13):
-                    return uData.getInt(this.U45);
+                    return uData.getShort(this.SpinSlot);
                 case (base+14):
-                    return uData.getInt(this.U46);
+                    return uData.getShort(this.CurveSlot);
                 case (base+15):
+                    return uData.getInt(this.DriveUp);
+                case (base+16):
+                    return uData.getInt(this.ItemDropUp);
+                case (base+17):
+                    return uData.getInt(this.ComboUp);
+                case (base+18):
+                    return uData.getInt(this.PangUp);
+                case (base+19):
+                    return uData.getInt(this.Exp);
+                case (base+20):
                     return uData.getInt(this.U47);
                 default:
                     // Return something that easily can identify missed values
@@ -194,7 +222,7 @@ public final class iffAuxPart extends iffClasses.iffBase {
         else {
             switch (colIndex) {
                 case base:
-                    this.U14 = uData.getShort((Integer)value);
+                    this.Amount = uData.getShort((Integer)value);
                     break;
                 case (base+1):
                     this.U33 = uData.getShort((Integer)value);
@@ -209,34 +237,34 @@ public final class iffAuxPart extends iffClasses.iffBase {
                     this.U36 = uData.getShort((Integer)value);
                     break;
                 case (base+5):
-                    this.U37 = uData.getShort((Integer)value);
+                    this.Power = uData.getShort((Integer)value);
                     break;
                 case (base+6):
-                    this.U38 = uData.getShort((Integer)value);
+                    this.Accuracy = uData.getShort((Integer)value);
                     break;
                 case (base+7):
-                    this.U39 = uData.getShort((Integer)value);
+                    this.Curve = uData.getShort((Integer)value);
                     break;
                 case (base+8):
-                    this.U40 = uData.getShort((Integer)value);
+                    this.ControlSlot = uData.getShort((Integer)value);
                     break;
                 case (base+9):
-                    this.U41 = uData.getShort((Integer)value);
+                    this.SpinSlot = uData.getShort((Integer)value);
                     break;
                 case (base+10):
-                    this.U42 = uData.getShort((Integer)value);
+                    this.DriveUp = uData.getShort((Integer)value);
                     break;
                 case (base+11):
-                    this.U43 = uData.getShort((Integer)value);
+                    this.ItemDropUp = uData.getShort((Integer)value);
                     break;
                 case (base+12):
-                    this.U44 = uData.getShort((Integer)value);
+                    this.ComboUp = uData.getShort((Integer)value);
                     break;
                 case (base+13):
-                    this.U45 = uData.getShort((Integer)value);
+                    this.PangUp = uData.getShort((Integer)value);
                     break;
                 case (base+14):
-                    this.U46 = uData.getShort((Integer)value);
+                    this.Exp = uData.getShort((Integer)value);
                     break;
                 case (base+15):
                     this.U47 = uData.getShort((Integer)value);
