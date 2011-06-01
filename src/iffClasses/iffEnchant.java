c9652b7af190924d96a7ead7269941cc7813f637
Hedkandi <chreadie@gmail.com>
2011-06-01 17:27:23 +0200

    Changed variable and columnnames

--- src/iffClasses/iffEnchant.java
+++ src/iffClasses/iffEnchant.java
@@ -13,15 +13,14 @@ import gens.uData;
  */
 public class iffEnchant {
     
-    // All bytes are +1 since hex fiend starts count at 0
-    public int numItemOne = 0; // 4 bytes - 1 or 0 - byte 0-3
-    public int itemOne = 0; // 4 bytes - byte 4-7
-    public int numItemTwo = 0; // 40 bytes - byte 8-47
-    public int itemTwo = 0; // 1 byte - byte 48
-    String[] colNames = new String[] {  "Num item?",
-                                    "Item?",
-                                    "Amount?",
-                                    "Price"};
+    public int Valid = 0;
+    public int EnchantID = 0;
+    public int Price = 0;
+    public int U1 = 0;
+    String[] colNames = new String[] {  "Valid",
+                                    "Enchant ID",
+                                    "Price",
+                                    "NA"};
     
     public iffEnchant(byte[] inData) {
         getItem(inData);
@@ -40,22 +39,22 @@ public class iffEnchant {
     }
     
     private void getItem(byte[] inData) {
-        numItemOne = uData.getInt(new byte[] {inData[0], inData[1], inData[2], inData[3]});
-        itemOne = uData.getInt(new byte[] {inData[4], inData[5], inData[6], inData[7]});
-        numItemTwo = uData.getInt(new byte[] {inData[8], inData[9], inData[10], inData[11]});
-        itemTwo = uData.getInt(new byte[] {inData[12], inData[13], inData[14], inData[15]});
+        Valid = uData.getInt(new byte[] {inData[0], inData[1], inData[2], inData[3]});
+        EnchantID = uData.getInt(new byte[] {inData[4], inData[5], inData[6], inData[7]});
+        Price = uData.getInt(new byte[] {inData[8], inData[9], inData[10], inData[11]});
+        U1 = uData.getInt(new byte[] {inData[12], inData[13], inData[14], inData[15]});
     }
 
     public Object getValue(int colIndex) {
         switch (colIndex) {
             case 0:
-                return uData.getLong(this.numItemOne);
+                return uData.getLong(this.Valid);
             case 1:
-                return uData.getLong(this.itemOne);
+                return uData.getLong(this.EnchantID);
             case 2:
-                return uData.getLong(this.numItemTwo);
+                return uData.getLong(this.Price);
             case 3:
-                return uData.getLong(this.itemTwo);
+                return uData.getLong(this.U1);
             default:
                 return "";
         }
@@ -64,16 +63,16 @@ public class iffEnchant {
     public void setValue(int colIndex, Object value) {
         switch (colIndex) {
             case 0:
-                this.numItemOne = uData.getInt((Long)value);
+                this.Valid = uData.getInt((Long)value);
                 break;
             case 1:
-                this.itemOne = uData.getInt((Long)value);
+                this.EnchantID = uData.getInt((Long)value);
                 break;
             case 2:
-                this.numItemTwo = uData.getInt((Long)value);
+                this.Price = uData.getInt((Long)value);
                 break;
             case 3:
-                this.itemTwo = uData.getInt((Long)value);
+                this.U1 = uData.getInt((Long)value);
                 break;
         }
     }
