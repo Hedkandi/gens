33834e08e464d0ebda9df28028ae97c918458d93
Hedkandi <chreadie@gmail.com>
2011-09-28 18:42:43 +0200

    Moved the filestreams out of the separate functions. Both reading and writing now uses the same streams.

--- src/gens/iffZipper.java
+++ src/gens/iffZipper.java
@@ -8,12 +8,16 @@ package gens;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Enumeration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipException;
 import java.util.zip.ZipFile;
+import java.util.zip.ZipOutputStream;
 
 /**
  *
@@ -22,9 +26,11 @@ import java.util.zip.ZipFile;
 public class iffZipper {
 
     private File fFile = null;
+    private FileOutputStream fis = null;
     private String sDir = "";
     private boolean bDoOverwrite = false;
     private ZipFile zf = null;
+    ZipOutputStream zout = null;
 
 
     public iffZipper(String sFilename) {
@@ -91,7 +97,42 @@ public class iffZipper {
         }
     }
 
-    public void doCompress() {
-
+    public void doCompress(byte[] data, String entry) {
+        //FileOutputStream fis = null;
+        try {
+            //fFile.createNewFile();
+            if (this.fis == null) {
+                this.fis = new FileOutputStream(this.fFile);
+            }
+            if (this.zout == null) {
+                this.zout = new 
+                ZipOutputStream(new BufferedOutputStream(fis));
+            }
+            ZipEntry ze = new ZipEntry(entry.substring(0, entry.indexOf(".")) + ".iff");
+            this.zout.putNextEntry(ze);
+            this.zout.write(data, 0, data.length);
+            this.zout.flush();
+            this.zout.closeEntry();
+            
+            
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(iffZipper.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(iffZipper.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            //try {
+                //fis.flush();
+                //fis.close();
+                System.out.println("next file");
+            //} catch (IOException ex) {
+            //    Logger.getLogger(iffZipper.class.getName()).log(Level.SEVERE, null, ex);
+            //}
+        }
+    }
+    
+    public void closeZip() throws IOException {
+        this.zout.finish();
+        this.zout.close();
+        this.fis.close();
     }
 }
